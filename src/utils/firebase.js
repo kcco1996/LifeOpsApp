d022e794ea6e42d9137827e30f7f97dbb53f814d
kcco1996 <kcco1996@gmail.com>
2026-01-27 22:27:19 +0000

    1

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -20,5 +20,3 @@ export const auth = getAuth(app);
 export const provider = new GoogleAuthProvider();
 export const db = getFirestore(app);
 
-// Offline cache (nice UX, not required for “survive reinstall”)
-enableIndexedDbPersistence(db).catch(() => {});
